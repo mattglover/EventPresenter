67cc3b0ea26900b4577de3ab54c5eaf898b96ff9
Matt Glover <matt@duchysoftware.com>
2017-06-06 21:56:29 +0100

    Initial commit : EventPresenterKit

---
+++ EventPresenterKit/EventPresenterKit/EventPresenterKit.h
@@ -0,0 +1,19 @@
+//
+//  EventPresenterKit.h
+//  EventPresenterKit
+//
+//  Created by Matt Glover on 06/06/2017.
+//  Copyright © 2017 Duchy Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EventPresenterKit.
+FOUNDATION_EXPORT double EventPresenterKitVersionNumber;
+
+//! Project version string for EventPresenterKit.
+FOUNDATION_EXPORT const unsigned char EventPresenterKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EventPresenterKit/PublicHeader.h>
+
+
---
+++ EventPresenterKit/EventPresenterKitTests/EventPresenterKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  EventPresenterKitTests.swift
+//  EventPresenterKitTests
+//
+//  Created by Matt Glover on 06/06/2017.
+//  Copyright © 2017 Duchy Software Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import EventPresenterKit
+
+class EventPresenterKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
